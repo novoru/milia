161e528608ef560d96d032c03b8fb6b32d4ec5de
Noboru Horita <61climb@gmail.com>
2020-07-04 21:45:09 +0900

    turn off echoing

--- milia.go
+++ milia.go
@@ -3,9 +3,26 @@ package main
 import (
 	"io"
 	"os"
+
+	"golang.org/x/sys/unix"
 )
 
+func enableRawMode() {
+	termios, err := unix.IoctlGetTermios(unix.Stdin, unix.TCGETS)
+	if err != nil {
+		panic(err)
+	}
+
+	mask := ^(unix.ECHO)
+	termios.Lflag &= uint32(mask)
+
+	if err := unix.IoctlSetTermios(unix.Stdin, unix.TCSETS, termios); err != nil {
+		panic(err)
+	}
+}
+
 func main() {
+	enableRawMode()
 	for {
 		buf := make([]byte, 1)
 		_, err := os.Stdin.Read(buf)
