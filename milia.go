9f994623b5b482cd9d96402a1c2fa5589a7c78c2
Noboru Horita <61climb@gmail.com>
2020-07-05 00:20:58 +0900

    display keypresses

--- milia.go
+++ milia.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"os"
+	"unicode"
 
 	"golang.org/x/sys/unix"
 )
@@ -12,6 +14,7 @@ func enableRawMode() {
 	if err != nil {
 		panic(err)
 	}
+
 	mask := ^(unix.ECHO | unix.ICANON)
 	termios.Lflag &= uint32(mask)
 
@@ -20,26 +23,33 @@ func enableRawMode() {
 	}
 }
 
-func disableRawMode(origTermios *unix.Termios) {
-	if err := unix.IoctlSetTermios(unix.Stdin, unix.TCSETS, origTermios); err != nil {
+func disableRawMode() {
+	termios, err := unix.IoctlGetTermios(unix.Stdin, unix.TCGETS)
+	if err != nil {
+		panic(err)
+	}
+
+	termios.Lflag |= uint32(unix.ECHO | unix.ICANON)
+
+	if err := unix.IoctlSetTermios(unix.Stdin, unix.TCSETS, termios); err != nil {
 		panic(err)
 	}
 }
 
 func main() {
-	origTermios, err := unix.IoctlGetTermios(unix.Stdin, unix.TCGETS)
-	if err != nil {
-		panic(err)
-	}
-
 	enableRawMode()
-	defer disableRawMode(origTermios)
+	defer disableRawMode()
 
 	for {
 		buf := make([]byte, 1)
 		_, err := os.Stdin.Read(buf)
 		if err == io.EOF || buf[0] == 'q' {
 			break
+		}
+		if unicode.IsControl(rune(buf[0])) {
+			fmt.Printf("%d\n", buf[0])
+		} else {
+			fmt.Printf("%d ('%c')\n", buf[0], buf[0])
 		}
 	}
 }
